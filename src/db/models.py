189e52afe39caa36d44b72ccd30a33dcca8d9a87
whoisjose569 <jose.vitor.araujo569@hotmail.com>
2024-04-02 14:49:25 -0300

    refatoração model

--- src/db/models.py
+++ src/db/models.py
@@ -5,5 +5,5 @@ class Category(Base):
     __tablename__ = 'categories'
     id = Column('id', Integer, primary_key=True, autoincrement=True)
     name = Column('name', String, nullable=False)
-    slug = Column(('slug', String, nullable=False))
+    slug = Column('slug', String, nullable=False)
     
